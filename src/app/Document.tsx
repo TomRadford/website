3003bfae511af2ab078b044b3614153328eb563c
Tom Radford <tom@theradford.com>
2025-11-09 12:08:56 +0200

    add prettier

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,8 +1,6 @@
 import styles from "./styles.css?url";
 
-export const Document: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => (
+export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
