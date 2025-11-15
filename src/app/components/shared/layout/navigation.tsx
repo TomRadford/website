935aa0439606e61b49e62b6ec5384aa83ac0d2e2
Tom Radford <tom@theradford.com>
2025-11-15 08:23:25 +0200

    nav change

--- src/app/components/shared/layout/navigation.tsx
+++ src/app/components/shared/layout/navigation.tsx
@@ -2,10 +2,10 @@ import { NavLink } from "./nav-link";
 
 export const Navigation = () => {
   return (
-    <nav className="flex gap-4 justify-center mt-8">
+    <nav className="flex gap-2 sm:gap-4 justify-center mt-8">
       <NavLink href="/">Posts</NavLink>
       <NavLink href="/about">About</NavLink>
-      <NavLink href="/uses">Uses</NavLink>
+      <NavLink href="/uses">Film</NavLink>
       <NavLink href="/contact">Contact</NavLink>
     </nav>
   );
