30d54ebc8249592525ce658291c100fac9f15079
Tom Radford <tom@theradford.com>
2025-11-15 08:23:36 +0200

    work on about page

--- src/app/pages/about/index.tsx
+++ src/app/pages/about/index.tsx
@@ -1,20 +1,31 @@
+import { Link } from "../../components/shared/link";
 import { Metadata } from "../../components/shared/metedata";
+import { Typography } from "../../components/shared/typography";
 
 export const AboutPage = () => {
   return (
     <>
       <Metadata
         title="About Me"
-        description="I'm a product engineer who's jazzed on creating meaningful problem oriented solutions."
-        publishDate="2025-11-09"
+        description="Software Developer who's jazzed on creating meaningful problem oriented solutions."
+        publishDate="2025-11-20"
         author="Tom Radford"
         slug="about"
       />
       <div className="space-y-4 text-center">
-        <h1 className="text-2xl font-bold">Hi, I'm Tom Radford!</h1>
-        <p>
-          I'm a product engineer who's jazzed on creating meaningful problem oriented solutions.
-        </p>
+        <Typography element="h3" weight="bold" size="h3">
+          Hi, I'm Tom Radford!
+        </Typography>
+        <Typography element="h4" weight="default" size="h4">
+          I'm a software developer who's jazzed on creating meaningful problem oriented solutions.
+        </Typography>
+        <Typography element="p" weight="default" size="p">
+          Currently, I work as a senior dev at{" "}
+          <Link target="_blank" href="https://teamgeek.io">
+            TeamGeek
+          </Link>
+          <a></a>.
+        </Typography>
       </div>
     </>
   );
