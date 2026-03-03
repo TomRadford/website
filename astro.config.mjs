cc9f42f587df3924e4c633abe3e0184c81af91f9
Tom Radford <tom@teamgeek.io>
2026-03-03 11:29:04 +0200

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,21 @@ import cloudflare from '@astrojs/cloudflare'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+
   adapter: cloudflare({
     imageService: 'cloudflare-binding',
     imagesBindingName: 'IMAGES',
     sessionKVBindingName: 'SESSIONS',
   }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 })
