8f577b65f9268b8cdf666f7bc20e9ab1325244fd
Tom Radford <tom@theradford.com>
2026-03-07 10:23:40 +0200

    rollback to astro v5 because its stable lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare'
-
-import tailwindcss from '@tailwindcss/vite'
-
-import mdx from '@astrojs/mdx';
-
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-
   adapter: cloudflare({
-    imageService: 'cloudflare-binding',
-    imagesBindingName: 'IMAGES',
-    sessionKVBindingName: 'SESSIONS',
-  }),
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [mdx()],
-})
+    platformProxy: {
+      enabled: true
+    },
+    imageService: "cloudflare"
+  })
+});
