9e6da9f48b8c42f5e6ef14f1fc0acd180d9ca6b2
Tom Radford <tom@theradford.com>
2026-03-07 21:24:47 +0200

    cleanup old worker file

--- src/worker.ts
+++
@@ -1,7 +0,0 @@
-import { handle } from '@astrojs/cloudflare/handler';
-
-export default {
-  async fetch(request, env, ctx) {
-    return handle(request, env, ctx);
-  },
-} satisfies ExportedHandler<Env>;
