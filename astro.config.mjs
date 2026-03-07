1966d3f457e3ed3ba99c29a3437e08159be6d5a2
Tom Radford <tom@theradford.com>
2026-03-07 22:28:23 +0200

    prefetch routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
